2210b6edea3b6ef914ad8522c0db0719a8848b3c
unknown <hanifowose@gmail.com>
2026-06-19 10:02:43 +0100

    (new update) started a javascript, navigation done

---
+++ Javascript folder/Index.js
@@ -0,0 +1,21 @@
+// using switch statement
+
+let day = "monday"
+
+if (day === "monday") {
+    console.log("Today is Monday");
+}else if (day === "tuesday") {
+    console.log("Today is Tuesday");
+}else if (day === "wednesday") {
+    console.log("Today is Wednesday");
+}else if (day === "thursday") {
+    console.log("Today is Thursday");
+}else if (day === "friday") {
+    console.log("Today is Friday");
+}else if (day === "saturday") {
+    console.log("Today is Saturday");
+}else if (day === "sunday") {
+    console.log("Today is Sunday");
+}else {
+    console.log("Invalid day");
+}
